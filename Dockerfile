6d286d7335303561dd091d8fb72d699f988a74a4
Cuongyd196 <Cuongyd196@gmail.com>
2022-02-21 19:00:51 +0700

    #init

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:14-alpine3.10 as build-step
+WORKDIR /app
+COPY package.json .
+RUN yarn install
+COPY . .
+RUN yarn run build
+
+FROM nginx:1.18.0-alpine
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+COPY --from=build-step app/build /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ReactJs App DEMO - Nginx - Dockerfile - docker-compose
+        </p>
+        <a
+          className="App-link"
+          href="https://cuongit.net"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CuongIT
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
